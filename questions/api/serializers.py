54d8e81de82560b43ee904ff7c20e6c640dfbc90
Stefan Gouldson <sgouldson@CASPIAN71.caspian.local>
2021-06-10 22:10:47 +0100

    SPA REST Complete

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -3,9 +3,10 @@ from questions.models import Answer, Question
 
 class AnswerSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
-    created_at = serializers.SerializerMethodField(read_only=True)
-    likes_count = serializers.SerializerMethodField(read_only=True)
-    user_has_voted = serializers.SerializerMethodField(read_only=True)
+    created_at = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+    user_has_voted = serializers.SerializerMethodField()
+    question_slug = serializers.SerializerMethodField() 
 
     class Meta:
         model = Answer
@@ -21,13 +22,16 @@ class AnswerSerializer(serializers.ModelSerializer):
         request = self.context.get("request")
         return instance.voters.filter(pk=request.user.pk).exists()
     
+    def get_question_slug(self, instance):
+        return instance.question.slug
+    
 
 class QuestionSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField(read_only=True)
-    created_at = serializers.SerializerMethodField(read_only=True)
+    created_at = serializers.SerializerMethodField()
     slug = serializers.SlugField(read_only=True)
-    answers_count = serializers.SerializerMethodField(read_only=True)
-    user_has_answered = serializers.SerializerMethodField(read_only=True)
+    answers_count = serializers.SerializerMethodField()
+    user_has_answered = serializers.SerializerMethodField()
 
     class Meta:
         model = Question
--- users/api/serializers.py
+++ users/api/serializers.py
@@ -4,5 +4,5 @@ from users.models import CustomUser
 class UserDisplaySerializer(serializers.ModelSerializer):
 
     class Meta: 
-        model = CustomUser,
-        fields = ["username"]
+        model = CustomUser
+        fields = ("username",)
